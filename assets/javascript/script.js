cdaf50b36dd8bd8fd97aca2c792f421dc4128d19
Brogan Carpenter <Brogancarpenter@hotmail.co.uk>
2024-06-02 15:28:20 +0000

    Start to add computersChoice generated answer script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,11 @@
-let choices = ["plant", "water", "fire"];
-let playersChoice = document.getElementById("playersChoice");
-let computersChoice = document.getElementById("computersChoice");
-let resultDisplay = document.getElementById("resultDisplay");
+const choices = ["plant", "water", "fire"];
+const playersChoice = document.getElementById("playersChoice");
+const computersChoice = document.getElementById("computersChoice");
+const resultDisplay = document.getElementById("resultDisplay");
 
 function playGame(playersChoice) {
 
-    let computersChoice = choices[Math.floor(Math.random() * 3];
+    const computersChoice = choices[Math.floor(Math.random() * 3)];
 
     console.log(computersChoice);
 
